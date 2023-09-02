5a039b9c6b22722d3ae93b8f4171dda22ace1811
Raghu <Nagamraghavendra8@gmail.com>
2023-09-02 22:45:52 +0530

    Inverted half numerical pyramid

---
+++ Pyramid_Patterns/Numerical_Pyramids/Inverted_Half_Pyramid/Solution.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    for(int row=1; row<=num; row++){
+        for(int col=1; col<=(num-row)+1; col++){
+            cout<<col;
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
