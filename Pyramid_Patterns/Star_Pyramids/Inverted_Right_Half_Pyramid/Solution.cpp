d2f8ae14024c5fd06f5e7da91053447481e83772
Raghu <Nagamraghavendra8@gmail.com>
2023-09-12 17:43:44 +0530

    Inverted Right Half Star Pyramid

---
+++ Pyramid_Patterns/Star_Pyramids/Inverted_Right_Half_Pyramid/Solution.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    for(int row=1;row<=num;row++){
+        for(int col=0;col<row;col++){
+            cout<<" ";
+        }
+        for(int col=row;col<=num;col++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
