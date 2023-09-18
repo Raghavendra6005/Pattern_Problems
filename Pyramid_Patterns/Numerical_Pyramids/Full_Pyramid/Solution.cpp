dd9a626b31a06dce8cec5ed4df5d63a9392d4bc8
Raghu <Nagamraghavendra8@gmail.com>
2023-09-18 23:50:01 +0530

    Numerical full pyramid

--- Pyramid_Patterns/Numerical_Pyramids/Full_Pyramid/Solution.cpp
+++ Pyramid_Patterns/Numerical_Pyramids/Full_Pyramid/Solution.cpp
@@ -3,5 +3,18 @@ using namespace std;
 int main(){
     int num;
     cin>>num;
-      
+    for(int row=1;row<=num;row++){
+        for(int col=0;col<num-row;col++){
+            cout<<"  ";
+        }
+        for(int col=row;col<=(2*row-1);col++){
+            cout<<col;
+            cout<<" ";
+        }
+        for(int col=(2*row-2);col>=row;col--){
+            cout<<col;
+            cout<<" ";
+        }
+        cout<<endl;
+    }return 0;    
 }
