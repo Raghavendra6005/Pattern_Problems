38315993b83ad8921f0e8da110ff7882ea0b1af7
Raghu <Nagamraghavendra8@gmail.com>
2023-10-13 11:50:13 +0530

    Different numerical Pattern

--- Ractangle_Patterns/Numerical_Patterns/Num_Pattern/Solution.cpp
+++ Ractangle_Patterns/Numerical_Patterns/Num_Pattern/Solution.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int row;
-    cin>>row;
-    for(int row)
+    int Number;
+    cin>>Number;
+    for(int row=1;row<=Number;row++){
+        for(int coloumn=row;coloumn<=Number;coloumn++){
+            cout<<coloumn<<" ";
+        }
+        for(int coloumn=1;coloumn<row;coloumn++){
+            cout<<coloumn<<" ";
+        }
+        cout<<endl; 
+    }
 }
